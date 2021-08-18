7bd1a0a686a655ba954496bdd2c227f9df32503b
MiNnNyINyI <MiNn_NyI@outlook.com>
2021-08-18 15:35:55 +0630

    First Commit

---
+++ MiNn.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void patterm1(int a);
+
+int main() {
+    int row=0;
+    printf("Enter Size Patterm!: ");
+    scanf("%d", &row);
+
+    patterm1(row);
+
+    return 0;
+}
+
+void patterm1(int a) {
+    int i, k;
+    for(i=0; i<a; i++) {
+        for(k=0; k<i; k++) {
+                printf("*");
+        } 
+        printf("\n");
+    }
+    printf("\n");
+}
